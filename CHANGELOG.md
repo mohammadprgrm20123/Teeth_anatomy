72c2db79adffde0bd7783cedbd59e19e576f69d5
Mohammad Kazeminejad <Kazeminejad@yahoo.com>
2024-09-24 14:58:45 +0330

    add changelog and readme

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+
+
+## 0.0.1
+* add readme file
+* add changelog file
--- lib/view/teeth_custom_paint.dart
+++ lib/view/teeth_custom_paint.dart
@@ -2,7 +2,6 @@
 
 
 import 'dart:math';
-import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart' as svg;
